ded64e48081b4a5fe2b6a4d26e4d3fb3de55ae94
Albertus Istora Pahala <albertus.istora@ti.ukdw.ac.id>
2021-04-02 14:56:58 +0700

    Added color list

---
+++ src/color.js
@@ -0,0 +1,11 @@
+export const colorPrimary = '#00ab6b';
+export const colorDanger = '#ee4a49';
+export const colorWhite = '#fff';
+export const colorGrey = '#aaa';
+export const colorDisabled = '#ccc';
+export const colorBackground = '#f9f9f9';
+export const colorBoxShadow = '#eee';
+export const colorBorder = '#ededed';
+export const colorTextGrey = '#949494';
+export const colorNotFound = '#f98f06';
+export const colorWarning = '#FFA726';

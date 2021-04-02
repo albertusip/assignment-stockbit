e74335dbf826ea4cfaa33a85b99f8bc373deaffc
Albertus Istora Pahala <albertus.istora@ti.ukdw.ac.id>
2021-04-02 14:57:11 +0700

    Added api js

---
+++ src/api/index.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+
+const API_KEY = 'faf7e5bb';
+const END_POINT = 'http://www.omdbapi.com/';
+
+const getMovies = async (value) => {
+    const { searchValue, page } = value;
+    try {
+        let url = `${END_POINT}?apikey=${API_KEY}&s=${searchValue}&page=${page}`;
+        const response = await axios.get(url);
+        return response.data;
+    } catch (error) {
+        throw error;
+    }
+}
+
+const getDetailMovie = async (value) => {
+    const { id } = value;
+    try {
+        const response = await axios.get(`${END_POINT}?apikey=${API_KEY}&i=${id}`);
+        return response.data;
+    } catch (error) {
+        throw error;
+    }
+}
+
+export {
+    getMovies,
+    getDetailMovie
+};
